70612b0ffde0dbbd9e154d12a40ff5f6fea60f0c
chaosnoneko <98882297+chaosnoneko@users.noreply.github.com>
2022-04-20 10:56:25 +0200

    Add files via upload

---
+++ day2/6conditionaloperators.js
@@ -0,0 +1,45 @@
+// < , > , <= , => , == abstract comparison , !== , === strict comparison
+console.log(1<2); // true
+console.log(1>2); // false
+console.log(2<2); // false 
+console.log(1<=2); // true
+console.log(2<=2); // true
+console.log(3>=3); // true
+console.log(3==3); // true
+console.log(3=="3"); //true javascript will not ckeck datatype 
+console.log(3==="3"); // false javascript will check datatype 
+console.log("hello" !== "World") //true 
+
+// get 2 numbers from user "10" and "3";
+if("10"===10){ //false
+    "10"+"3"
+}else{}
+
+let name = "John"; //expression
+let sum = 2+5; // statement
+// statement : instruction to javascript to do something
+// expression : value itself or anything will become a value is called expression  
+let sum = function // not an expression
+// "hello" + "wWorld" => hello World
+// 1+2+3+4 => 10 artematic expression
+// 1<2 => conditional expression
+//1<2 && 2>1 => logical expression
+
+let total; // statement 
+const name = "robert"
+
+// logical operators
+// && AND , || OR , ! NOT
+// && 0 1 => 0,
+//    1 0 => 0,
+//    1 1 => 1,
+//    0 0 => 0
+1>2 and 4>5
+console.log(first)
+console.log (1<2 && 4>5 && 2>3) // false => 1 * 0 * 0 = 0
+console.log (1<2 || 4>5 || 2>3) // true => 1 + 0 + 0 = 1
+console.log(!true) // false
+
+const experience = 5;
+const degree = true;
+console.log(experience >=5 || degree===true)
